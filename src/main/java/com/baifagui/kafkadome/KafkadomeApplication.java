17c8453dfec81554a652d23cff61cb8ac6b4ce98
baifagui <65336046@qq.com>
2019-10-04 21:39:45 +0800

    创建项目

---
+++ src/main/java/com/baifagui/kafkadome/KafkadomeApplication.java
@@ -0,0 +1,13 @@
+package com.baifagui.kafkadome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkadomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkadomeApplication.class, args);
+    }
+
+}
